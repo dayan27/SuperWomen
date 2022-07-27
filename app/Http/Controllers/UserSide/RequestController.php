6b98ec9dfb9430fcfc0773a0ed182f0bec6b9ee7
dayan27 <dayanyenesew@gmail.com>
2022-07-27 13:51:09 +0300

    commit after error

--- app/Http/Controllers/UserSide/RequestController.php
+++ app/Http/Controllers/UserSide/RequestController.php
@@ -14,7 +14,7 @@ class RequestController extends Controller
         $men_req=new MentorRequest();
         $men_req->user_id=$user->id;
         $men_req->mentor_id=$mentor_id;
-        $men_req->request_message='$request->request_message';
+        $men_req->request_message=$request->message;
         $men_req->save();
 
         return response()->json('success',200);
